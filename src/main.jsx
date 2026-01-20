69642d845d39470cfb9d0abdab7aa7f81872af43
ByteMe6 <120343461+ByteMe6@users.noreply.github.com>
2026-01-20 21:34:07 +0200

    HashRoutes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import 'normalize.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
